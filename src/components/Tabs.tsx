bbae58275bfd2f8c59ce323dc9d91856c2af6a27
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-03 22:33:42 -0300

    refac: change tabs logic

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -2,20 +2,21 @@ import type { TodoProps } from "../types/Types";
 
 export default function Tabs({ todos }: TodoProps) {
 	const tabs = ["All", "Open", "Completed"];
+
 	return (
-		<nav className="flex justify-between md:gap-5 md:justify-center font-bold">
+		<nav className="flex justify-between md:gap-5 md:justify-center font-bold text-md">
 			{tabs.map((tab, tabIndex) => {
 				const numOfTasks =
 					tab === "All"
 						? todos.length
-						: tab === "Open"
+						: tab === "Completed"
 							? todos.filter((val) => !val.complete).length
 							: todos.filter((val) => val.complete).length;
 				return (
 					<button
 						type="button"
 						key={tabIndex}
-						className="bg-amber-700 rounded-2xl p-2 hover:bg-amber-600 hover:scale-115"
+						className="bg-amber-700 rounded-2xl px-1 py-1.5 md:p-3 hover:bg-amber-600 hover:scale-115"
 					>
 						<div>{`${tab} (${numOfTasks})`}</div>
 					</button>
