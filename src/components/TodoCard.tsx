2fa7b8d0fe268fbb027dc2bd47d477ac3b22a7cf
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-03 22:32:34 -0300

    feat: Add TodoCard component

---
+++ src/components/TodoCard.tsx
@@ -0,0 +1,20 @@
+import type { TodoTaskType } from "../types/Types";
+
+export default function TodoCard({ todo }: TodoTaskType) {
+	return (
+		<div className="flex flex-col gap-1 bg-blue-600 rounded-2xl font-bold p-5">
+			<p className="text-base md:text-xl">{todo.input}</p>
+			<div className="flex gap-4 items-center text-sm">
+				<button
+					className="bg-emerald-400 p-1 rounded-md disabled:bg-emerald-700 hover:scale-110"
+					disabled={todo.complete}
+				>
+					Complete
+				</button>
+				<button className="bg-red-400 p-1 rounded-md hover:scale-110">
+					Delete
+				</button>
+			</div>
+		</div>
+	);
+}
