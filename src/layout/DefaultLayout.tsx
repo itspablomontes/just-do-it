8fa2e444f85437710c92e2b0fd37397699e63975
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-03 22:32:55 -0300

    style: change layout

--- src/layout/DefaultLayout.tsx
+++ src/layout/DefaultLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router-dom";
 
 export default function DefaultLayout() {
 	return (
-		<>
+		<div className="bg-zinc-900 min-h-screen">
 			<Header />
 			<Outlet />
-		</>
+		</div>
 	);
 }
