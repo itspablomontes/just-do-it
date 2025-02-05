24c2be96391da0420ebf84af5c6ddfa3ff672e54
Pablo Montes <169383657+itspablomontes@users.noreply.github.com>
2025-02-05 20:05:27 -0300

    style: correct overflowing

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -3,15 +3,17 @@ import type { TodoCardProps } from "../types/Types";
 export default function TodoCard({
 	todo,
 	todoIndex,
+	handleCompleteTodo,
 	handleDeleteTodo,
 }: TodoCardProps) {
 	return (
-		<div className="flex flex-col gap-1 bg-blue-600 rounded-2xl font-bold p-5">
-			<p className="text-base md:text-xl">{todo.input}</p>
-			<div className="flex gap-4 items-center text-sm">
+		<div className="flex flex-col gap-1 bg-blue-600 rounded-2xl font-bold p-5 w-2xs break-words">
+			<p className="text-base md:text-xl overflow-scroll">{todo.input}</p>
+			<div className="flex gap-4 items-center text-sm max-h-28	">
 				<button
 					className="bg-emerald-400 p-1 rounded-md disabled:bg-emerald-700 hover:scale-110"
 					disabled={todo.complete}
+					onClick={() => handleCompleteTodo(todoIndex)}
 				>
 					Complete
 				</button>
